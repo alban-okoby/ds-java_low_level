d7df491c084ad7bb70fa3471a518edfb33f74d05
Cerveau++ <77627170+alban-okoby@users.noreply.github.com>
2023-06-05 03:45:58 +0000

    First program done ✅

---
+++ hello_world/src/HelloWorldApplication.java
@@ -0,0 +1,6 @@
+
+public class HelloWorldApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello World! Good job, your first program is running ✅🚀");
+    }
+}
